a15c23cdf4a34975d857d0a538c84dcae8ebf9e6
Elvg Ovadykov <oelvg@mail.ru>
2023-02-12 21:55:16 +0300

    Добавление модальных окон изменения паролья аватара

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,4 +1,6 @@
-const button = document.querySelector("button[type='submit']");
+import { closeModal, openModal } from "../../utils/modal";
+
+const button = document.querySelector("#saveChangesButton");
 
 button.onclick = function (event) {
   event.preventDefault();
@@ -6,3 +8,19 @@ button.onclick = function (event) {
   a.setAttribute("href", "/");
   a.click();
 };
+
+const profileBlockAvatar = document.querySelector(".profile-avatar-block");
+const uploadAvatarModal = document.querySelector("#uploadAvatarModal");
+const saveAvatarButton = document.querySelector("#saveAvatarButton");
+
+profileBlockAvatar.onclick = () => openModal(uploadAvatarModal);
+uploadAvatarModal.onclick = () => closeModal(uploadAvatarModal);
+saveAvatarButton.onclick = () => closeModal(uploadAvatarModal);
+
+const changePasswordButton = document.querySelector("#changePasswordButton");
+const changePasswordModal = document.querySelector("#changePasswordModal");
+const savePasswordButton = document.querySelector("#savePasswordButton");
+
+changePasswordButton.onclick = () => openModal(changePasswordModal);
+changePasswordModal.onclick = () => closeModal(changePasswordModal);
+savePasswordButton.onclick = () => closeModal(changePasswordModal);
---
+++ src/utils/modal.js
@@ -0,0 +1,8 @@
+export function closeModal(modal) {
+  modal.style.display = "none";
+}
+
+export function openModal(modal) {
+  console.log(modal);
+  modal.style.display = "block";
+}
