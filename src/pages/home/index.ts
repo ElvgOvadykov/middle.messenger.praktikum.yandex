88001710820d4a6daab32ea70ac553179fea6c0a
Elvg Ovadykov <oelvg@mail.ru>
2023-02-23 20:06:23 +0300

    Доработал страницу заглушку для перехода на другие страницы

---
+++ src/pages/home/index.ts
@@ -0,0 +1,54 @@
+import Block from "@utils/Block";
+import Link from "@components/Link";
+
+import getGoToPageFunction from "@utils/getGoToPageFunction";
+
+import template from "./index.hbs";
+
+export default class HomePage extends Block {
+  init() {
+    this.childrens.signUpPageLink = new Link({
+      linkHref: "",
+      linkTitle: "Регистрация",
+      events: {
+        click: getGoToPageFunction("signUp"),
+      },
+    });
+
+    this.childrens.loginPageLink = new Link({
+      linkHref: "",
+      linkTitle: "Авторизация",
+      events: {
+        click: getGoToPageFunction("login"),
+      },
+    });
+
+    // this.childrens.profilePageLink = new Link({
+    //   linkHref: "",
+    //   linkTitle: "Профиль",
+    //   events: {
+    //     click: getGoToPageFunction("profile"),
+    //   },
+    // });
+
+    this.childrens.notFoundPageLink = new Link({
+      linkHref: "",
+      linkTitle: "404 ошибка",
+      events: {
+        click: getGoToPageFunction("notFound"),
+      },
+    });
+
+    this.childrens.serverErrorPageLink = new Link({
+      linkHref: "",
+      linkTitle: "5XX ошибка",
+      events: {
+        click: getGoToPageFunction("serverError"),
+      },
+    });
+  }
+
+  protected render() {
+    return this.compile(template, this.props);
+  }
+}
--- src/utils/renderDom.ts
+++ src/utils/renderDom.ts
@@ -1,9 +1,15 @@
 import LoginPage from "@pages/Login";
 import SignUpPage from "@pages/SignUp";
+import NotFoundErrorPage from "@pages/404";
+import ServerErrorPage from "@pages/500";
+import HomePage from "@pages/home";
 
 export const ROUTES = {
   login: LoginPage,
   signUp: SignUpPage,
+  notFound: NotFoundErrorPage,
+  serverError: ServerErrorPage,
+  home: HomePage,
 };
 
 export default function renderDOM(route: keyof typeof ROUTES) {
