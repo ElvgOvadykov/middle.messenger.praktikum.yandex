93a5d6d3f789bab8d2ac8d6a2fc7cabcadcfdef9
Elvg Ovadykov <oelvg@mail.ru>
2023-02-26 00:50:08 +0300

    добавление компонента для отображения даты последнего сообщения в чате

---
+++ src/components/ChatItem/components/LastMessageDate/index.ts
@@ -0,0 +1,39 @@
+import Block from "@utils/Block";
+
+import template from "./lastMessageDate.hbs";
+
+import "./style.scss";
+
+interface ILastMessageDateProps {
+  date: Date;
+}
+
+type ILastMessageDateExtendedProps = ILastMessageDateProps & {
+  dateText: string;
+};
+
+export default class LastMessageDate extends Block<ILastMessageDateExtendedProps> {
+  constructor(props: ILastMessageDateProps) {
+    let dateText = "";
+    const today = new Date();
+
+    const { date } = props;
+
+    if (props.date.toDateString() === today.toDateString()) {
+      dateText = `${date.getHours()}:${date.getMinutes()}`;
+    } else {
+      dateText = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+    }
+
+    const extentedProps: ILastMessageDateExtendedProps = {
+      date: props.date,
+      dateText,
+    };
+
+    super(extentedProps);
+  }
+
+  protected render(): DocumentFragment {
+    return this.compile(template, this.props);
+  }
+}
