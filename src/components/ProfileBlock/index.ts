f71bf37f726be2885fa3600fd974720c026a30da
Elvg Ovadykov <oelvg@mail.ru>
2023-02-26 20:56:39 +0300

    Добавление компонента показа управлением профилем внизу чатов

---
+++ src/components/ProfileBlock/index.ts
@@ -0,0 +1,46 @@
+import Button, { ButtonSize, ButtonColor } from "@components/Button";
+import Block from "@utils/Block";
+
+import getGoToPageFunction from "@utils/getGoToPageFunction";
+import renderDOM from "@utils/renderDom";
+
+import template from "./profileBlock.hbs";
+
+import "./style.scss";
+
+interface IProfileBlockProps {
+  profile: {
+    firstName: string;
+    secondName: string;
+  };
+}
+
+export default class ProfileBlock extends Block<IProfileBlockProps> {
+  constructor(props: IProfileBlockProps) {
+    super(props);
+  }
+
+  protected init(): void {
+    this.childrens.buttonLogout = new Button({
+      contentValue: "",
+      type: "button",
+      size: ButtonSize.Small,
+      color: ButtonColor.White,
+      events: {
+        click: getGoToPageFunction("login"),
+      },
+    });
+  }
+
+  componentDidMount(): void {
+    const block = document.querySelector(".profile-block__info");
+
+    block?.addEventListener("click", (event: Event) => {
+      renderDOM("profile");
+    });
+  }
+
+  protected render(): DocumentFragment {
+    return this.compile(template, this.props);
+  }
+}
