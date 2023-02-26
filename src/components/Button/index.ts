cd4b8a42396f18f331d4e1972d420a7f77532f87
Elvg Ovadykov <oelvg@mail.ru>
2023-02-26 18:40:50 +0300

    Переработка компонента кнопки

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,15 +1,40 @@
 import Block from "@utils/Block";
 import template from "./button.hbs";
 
-interface ButtonProps {
-  type: string;
-  contentValue: string;
-  events?: Record<string, Function>
+export enum ButtonSize {
+  FullWith = "button__size-full-width",
+  Medium = "button__size-medium-width",
+  Small = "button__size-small-width",
 }
 
-export default class Button extends Block<ButtonProps> {
-  constructor(props: ButtonProps) {
-    super(props);
+export enum ButtonColor {
+  Green = "button__color-green",
+  White = "button__color-white",
+}
+
+interface IButtonProps {
+  type: string;
+  contentValue: string;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  events?: Record<string, Function>;
+}
+
+type TButtonExtendedProps = IButtonProps & {
+  classes: Array<string>;
+};
+
+export default class Button extends Block<TButtonExtendedProps> {
+  constructor(props: IButtonProps) {
+    const extendedProps: TButtonExtendedProps = {
+      ...props,
+      classes: [""],
+    };
+
+    extendedProps.classes.push(props.size ?? ButtonSize.FullWith);
+    extendedProps.classes.push(props.color ?? ButtonColor.Green);
+
+    super(extendedProps);
   }
 
   protected render(): DocumentFragment {
