6fe924eb7fc3c727c820b0f5c66c386f52ff60f6
Elvg Ovadykov <oelvg@mail.ru>
2023-03-25 17:32:49 +0300

    Добавление Content-Security-Policy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
 const express = require("express");
-require("dotenv").config({ // eslint-disable-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config({
 	path: "./.env",
 });
 
@@ -8,6 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./dist"));
+
+app.use((req, res, next) => {
+	res.setHeader(
+		"Content-Security-Policy",
+		`default-src 'self'; 
+		 font-src 'self';
+		 img-src 'self';
+		 script-src 'self';
+		 style-src 'self';
+		 frame-src 'self'`,
+	);
+	next();
+});
 
 app.listen(PORT, () => {
 	// eslint-disable-next-line no-console
